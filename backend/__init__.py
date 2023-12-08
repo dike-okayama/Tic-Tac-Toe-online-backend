d6ae511eb7b75f451d3693a6d417ebd1b405a08f
dike-okayama <lawliet.d.k.m@gmail.com>
2023-12-08 17:39:08 +0900

    Replace the Python project with the Rust one

--- backend/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .server import handle_client
-
-__all__ = ["handle_client"]
--- backend/src/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .client import Client
-from .game import TicTacToe
-from .room import Room
-
-__all__ = ["Client", "Room", "TicTacToe"]
