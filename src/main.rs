bc1b21512636c66413047a30cf248eefe73a393d
dike-okayama <lawliet.d.k.m@gmail.com>
2023-12-30 16:13:00 +0900

    Enable SSL/TLS

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod websocket_session;
 use actix::{Actor, Addr};
 use actix_web::{get, web::Data, web::Payload, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use dotenvy::dotenv;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use websocket_actor::WebsocketActor;
 use websocket_session::WebsocketSession;
 
@@ -21,14 +23,43 @@ pub async fn handle_connection(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host = std::env::var("HOST").unwrap_or("0.0.0.0".to_string());
+    let port = std::env::var("PORT").unwrap_or("8000".to_string());
+
     let websocket_server = WebsocketActor::default().start();
 
-    HttpServer::new(move || {
-        App::new()
-            .service(handle_connection)
-            .app_data(Data::new(websocket_server.clone()))
-    })
-    .bind("0.0.0.0:5174")?
-    .run()
-    .await
+    if std::env::var("PRIVATE_KEY_FILE").is_err()
+        || std::env::var("CERTIFICATE_CHAIN_FILE").is_err()
+    {
+        HttpServer::new(move || {
+            App::new()
+                .service(handle_connection)
+                .app_data(Data::new(websocket_server.clone()))
+        })
+        .bind(format!("{}:{}", host, port))?
+        .run()
+        .await
+    } else {
+        let private_key = std::env::var("PRIVATE_KEY_FILE").unwrap();
+        let certificate_chain = std::env::var("CERTIFICATE_CHAIN_FILE").unwrap();
+
+        let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        builder
+            .set_private_key_file(private_key, SslFiletype::PEM)
+            .unwrap();
+        builder
+            .set_certificate_chain_file(certificate_chain)
+            .unwrap();
+
+        HttpServer::new(move || {
+            App::new()
+                .service(handle_connection)
+                .app_data(Data::new(websocket_server.clone()))
+        })
+        .bind_openssl(format!("{}:{}", host, port), builder)?
+        .run()
+        .await
+    }
 }
