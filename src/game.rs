4b08a008076cfe5518d6676406e6332951729345
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-09-10 18:08:44 +0900

    Make the game fun

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,11 @@
+use std::collections::VecDeque;
+
 type BoardType = [i8; 9];
 
 pub struct TicTacToe {
     pub elapsed_turn: u8,
     pub board: BoardType,
+    history: VecDeque<u8>,
 }
 
 impl TicTacToe {
@@ -23,6 +26,7 @@ impl TicTacToe {
         TicTacToe {
             elapsed_turn: 0,
             board: [-1; 9],
+            history: VecDeque::with_capacity(6),
         }
     }
     pub fn is_ended(&self) -> bool {
@@ -36,7 +40,7 @@ impl TicTacToe {
             };
         }
 
-        if self.elapsed_turn == 9 {
+        if self.board.iter().all(|&x| x != -1) {
             return true;
         }
 
@@ -77,6 +81,12 @@ impl TicTacToe {
 
         self.board[pos] = (self.elapsed_turn as i8) % 2;
         self.elapsed_turn += 1;
+        if self.history.len() == 6 {
+            if let Some(target) = self.history.pop_front() {
+                self.board[target as usize] = -1;
+            }
+        }
+        self.history.push_back(pos as u8);
 
         true
     }
@@ -84,5 +94,6 @@ impl TicTacToe {
     pub fn reset(&mut self) {
         self.elapsed_turn = 0;
         self.board = [-1; 9];
+        self.history.clear();
     }
 }
